1ec93bd35d74e578e511592067351c801cce4347
anhhuynh <anhhuynh@kms-technology.com>
2021-05-20 11:06:32 +0700

    Initial commit.

---
+++ cirrus/mysql/docker-entrypoint-initdb.d/1_create_database.sql
@@ -0,0 +1,3 @@
+CREATE USER IF NOT EXISTS 'anhhuynh_practice'@'%' IDENTIFIED BY 'anhhuynh_practice123';
+CREATE SCHEMA IF NOT EXISTS anhhuynh_practice CHARACTER SET = utf8;
+GRANT ALL PRIVILEGES ON anhhuynh_practice.* to 'anhhuynh_practice';
