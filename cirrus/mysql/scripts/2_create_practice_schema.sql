6173507b0a4705e1af03b9f6a6bc3e88aa39340b
anhhuynh <anhhuynh@kms-technology.com>
2021-06-03 16:59:41 +0700

    Added scripts to create/insert data into event table.

--- cirrus/mysql/scripts/2_create_practice_schema.sql
+++ cirrus/mysql/scripts/2_create_practice_schema.sql
@@ -12,3 +12,10 @@ create table message (
     created_at timestamp default current_timestamp,
     constraint message_pk primary key (id),
     constraint message_author_fk foreign key (author_id) references author(id));
+
+create table event (
+    id int(10) not null auto_increment,
+	message_id int,
+    constraint event_pk primary key (id),
+    constraint event_message_fk foreign key (message_id) references message(id));
+	
--- cirrus/mysql/scripts/3_init_data_practice_schema.sql
+++ cirrus/mysql/scripts/3_init_data_practice_schema.sql
@@ -14,3 +14,7 @@ insert into message(author_id, content) values (2, 'msg 2');
 insert into message(author_id, content) values (3, 'msg 2');
 insert into message(author_id, content) values (1, 'msg 3');
 insert into message(author_id, content) values (1, 'msg 4');
+insert into message(author_id, content) values (3, 'kms msg 1');
+
+
+insert into event (message_id) values (9);
